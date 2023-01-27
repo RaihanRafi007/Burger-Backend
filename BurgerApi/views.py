f6c6414930a64966e7bcf61f046e35cf16b289d9
RaihanRafi007 <rrafi0570@gamail.com>
2023-01-28 00:00:35 +0600

    first commit

---
+++ BurgerApi/views.py
@@ -0,0 +1,9 @@
+from rest_framework.viewsets import ModelViewSet
+
+from BurgerApi.models import UserProfile
+from BurgerApi.serializers import UserProfileSerializer
+
+# Create your views here.
+class UserProfileViewSet(ModelViewSet):
+    serializer_class = UserProfileSerializer
+    queryset = UserProfile.objects.all()
